e65376dbcacf5a2dfe5c6648704ad04072d091e8
0luv69 <baniyarujal@gmail.com>
2024-06-01 10:15:59 +0545

    Second Init

--- Bima_project/Bima_project/settings.py
+++ Bima_project/Bima_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-hpdqdf)zb3+t%i6bt*o1+ptk5c_m$stxm)^dni#**b_gue2qq4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1' ]
 
 
 # Application definition
@@ -37,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 'tailwind',
+    # 'theme',
+    # 'django_browser_reload',
+
+
+    'Bimapage',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +62,7 @@ ROOT_URLCONF = 'Bima_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,8 +124,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS= [os.path.join(BASE_DIR, 'public/static')]
+
+MEDIA_URL= 'media/'
+MEDIA_ROOT= os.path.join(BASE_DIR, 'public/media')
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+# # for tailwid setup
+
+# TAILWIND_APP_NAME = 'theme'
+
+# INTERNAL_IPS = [
+#     '127.0.0.1',  # IPv4 localhost
+#     'localhost',  # localhost as hostname
+# ]
+
+# NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
--- Bima_project/Bimapage/models.py
+++ Bima_project/Bimapage/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from django.utils.text import slugify
+
+
+
+class Appoinment(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE)
+    # opd= models.ForeignKey (Opd)
+    Hospital_CHOICES= (
+        ('H1', 'H1'),
+        ('H2', 'H2'),
+    )
+    Hospital = models.CharField(max_length=20, choices=Hospital_CHOICES, default='H1')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # medicine_token = models.ForeignKey(Medicine_token)
+    appoinment_status= models.BooleanField(default=True)
+
+
+
+class Hospital(models.Model):
+    registration_num= models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    hos_name= models.CharField(max_length=100)
+    hos_address= models.CharField(max_length=200)
+    Hos_type= (
+        ('gov', 'government'),
+        ('private', 'private'),
+    )
+    hos_type= models.CharField(max_length=30, choices=Hos_type, default= 'gov')
 
 
