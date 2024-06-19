dce7a80fd097cd077651e7bf3d3946bd2dbffd12
0luv69 <baniyarujal@gmail.com>
2024-06-19 16:51:27 +0545

    Re-edit

--- Bima_project/Bima_project/settings.py
+++ Bima_project/Bima_project/settings.py
@@ -130,7 +130,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kathmandu'
 
 USE_I18N = True
 
@@ -142,6 +142,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'public/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL= 'media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'public/media')
