cf46dcbe4e7ee5fa75f7c57cfdc6f45d32787782
0luv69 <baniyarujal@gmail.com>
2024-09-22 19:13:26 +0545

    updated the bima project to lunch

--- Bima_project/Bima_project/settings.py
+++ Bima_project/Bima_project/settings.py
@@ -12,21 +12,21 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
+DEBUG = str(os.getenv('DEBUG')).lower() == 'true'
+PRODUCTION_ENV = str(os.getenv('PRODUCTION_ENV')).lower() == 'true'
+USE_SQLITE = str(os.getenv('USE_SQLITE')).lower() == 'true'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hpdqdf)zb3+t%i6bt*o1+ptk5c_m$stxm)^dni#**b_gue2qq4'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.67' ]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'tailwind',
     'theme',
     'django_browser_reload',
+
+    'whitenoise.runserver_nostatic',
 
 
     'Bimapage',
@@ -63,6 +65,8 @@ NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -98,13 +102,20 @@ WSGI_APPLICATION = 'Bima_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
+if USE_SQLITE:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+elif PRODUCTION_ENV:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.getenv('POSTGRES_URL')
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -141,8 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS= [os.path.join(BASE_DIR, 'public/static')]
+if PRODUCTION_ENV:
+    STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS= [os.path.join(BASE_DIR, 'public/static/'), os.path.join(BASE_DIR, 'theme', 'static', 'css', 'dist'),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL= 'media/'
